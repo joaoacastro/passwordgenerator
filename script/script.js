f029dd3be6d790fd56a6d6c13870a5de64d219d9
JoaoACastro <joaoaccastro@gmail.com>
2022-08-17 08:11:49 -0300

    update script

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ let containerPassword = document.querySelector("#container-password");
 
 let letters = document.querySelector("#letters");
 let numbers = document.querySelector("#numbers");
-let specialCharacters = document.querySelector("#specialCharacters"); 
+let specialCharacters = document.querySelector("#specialCharacters");
 
 let charsetUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let charsetLowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -21,6 +21,12 @@ let charset =
   charsetSpecialCharacters +
   charsetNumbers;
 
+let charsetLetters = charsetUpperCase + charsetLowerCase;
+let charsetLettersAndNumbers = charsetLetters + charsetNumbers;
+let charsetLettersAndSpecialCharacters =
+  charsetLetters + charsetSpecialCharacters;
+let charsetNumbersAndSpecialCharacters = charsetNumbers + charsetSpecialCharacters;
+
 let newPassword = "";
 
 sizePassword.innerHTML = sliderElement.value;
@@ -30,14 +36,98 @@ sliderElement.oninput = function () {
 };
 
 function generatePassword() {
-  let pass = "";
-  for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
-    pass += charset.charAt(Math.floor(Math.random() * n));
+  if (letters.checked) {
+    let pass = "";
+    for (let i = 0, n = charsetLetters.length; i < sliderElement.value; ++i) {
+      pass += charsetLetters.charAt(Math.floor(Math.random() * n));
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    newPassword = pass;
   }
 
-  containerPassword.classList.remove("hide");
-  password.innerHTML = pass;
-  newPassword = pass;
+  if (numbers.checked) {
+    let pass = "";
+    for (let i = 0, n = charsetNumbers.length; i < sliderElement.value; ++i) {
+      pass += charsetNumbers.charAt(Math.floor(Math.random() * n));
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    newPassword = pass;
+  }
+
+  if (specialCharacters.checked) {
+    let pass = "";
+    for (
+      let i = 0, n = charsetSpecialCharacters.length;
+      i < sliderElement.value;
+      ++i
+    ) {
+      pass += charsetSpecialCharacters.charAt(Math.floor(Math.random() * n));
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    newPassword = pass;
+  }
+
+  if (letters.checked && numbers.checked) {
+    let pass = "";
+    for (
+      let i = 0, n = charsetLettersAndNumbers.length;
+      i < sliderElement.value;
+      ++i
+    ) {
+      pass += charsetLettersAndNumbers.charAt(Math.floor(Math.random() * n));
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    newPassword = pass;
+  }
+
+  if (letters.checked && specialCharacters.checked) {
+    let pass = "";
+    for (
+      let i = 0, n = charsetLettersAndSpecialCharacters.length;
+      i < sliderElement.value;
+      ++i
+    ) {
+      pass += charsetLettersAndSpecialCharacters.charAt(
+        Math.floor(Math.random() * n)
+      );
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    newPassword = pass;
+  }
+
+  if (numbers.checked && specialCharacters.checked) {
+    let pass = "";
+    for (
+      let i = 0, n = charsetNumbersAndSpecialCharacters.length;
+      i < sliderElement.value;
+      ++i
+    ) {
+      pass += charsetNumbersAndSpecialCharacters.charAt(
+        Math.floor(Math.random() * n)
+      );
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    newPassword = pass;
+  }
+
+  if (letters.checked && numbers.checked && specialCharacters.checked) {
+    let pass = "";
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+      pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    newPassword = pass;
+  }
+  if (!letters.checked && !numbers.checked && !specialCharacters.checked) {
+    alert("Please select some of the items! 🤔")
+  }
 }
 
 function copyPassword() {
